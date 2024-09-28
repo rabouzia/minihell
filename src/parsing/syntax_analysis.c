3d3afa73213478978cfdaa92b282035e68717dba
rabouzia <therwc93@gmail.com>
2024-09-28 21:09:58 +0200

    token, automate done

--- src/parsing/syntax_analysis.c
+++ src/parsing/syntax_analysis.c
@@ -6,7 +6,7 @@
 /*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 19:54:55 by rabouzia          #+#    #+#             */
-/*   Updated: 2024/09/28 20:33:48 by rabouzia         ###   ########.fr       */
+/*   Updated: 2024/09/28 20:52:57 by rabouzia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ bool syntax_analysis(t_minishell *minishell)
 
 bool init_state(t_token *token)
 {
-	if (!token)
-		return (1);
 	if (token->type == WORD)
 		return(word_state(token->next));
 	if (token->type == REDIR)
@@ -30,15 +28,33 @@ bool init_state(t_token *token)
 
 bool word_state(t_token *token)
 {
-	
+	if (!token)
+		return (1);
+	if (token->type == WORD)
+		return (word_state(token->next));
+	if (token->type == REDIR)
+		return (redir_state(token->next));
+	if (token->type == PIPE)
+		return (pipe_state(token->next));
+	return (0);
 }
 
 bool redir_state(t_token *token)
 {
-
+	if (!token)
+		return (0);
+	if (token->type == WORD)
+		return (word_state(token->next));
+	return (0);
 }
 
 bool pipe_redir(t_token *token)
 {
-	
+	if (!token)
+		return (0);
+	if (token->type == WORD)
+		return (word_state(token->next));
+	if (token->type == REDIR)
+		return (redir_state(token->next));
+	return (0);
 }
