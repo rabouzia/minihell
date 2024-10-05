3c5b874f7b26f51ac9c7c700482272a03b23151c
Ramzy Zoldik <rabouzia@student.42.fr>
2024-10-06 01:57:04 +0200

    starting pipes

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: ramzerk <ramzerk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 19:14:20 by rabouzia          #+#    #+#             */
-/*   Updated: 2024/10/06 01:35:39 by ramzerk          ###   ########.fr       */
+/*   Updated: 2024/10/06 01:56:40 by ramzerk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,28 @@
 
 bool	exec(t_minishell *minishell)
 {
-	
-	char **arg;
-	arg = minishell->command->arguments;
-	// if (!ft_pipe())
-	// 	return (0);
-	// if (fork())
-	// 	return (0);
-	// if (!redir())
-	// 	return (0);
-	if (!builtins(minishell, arg))
-		return (0);
-	// if (!bin_cmd())
-	// 	return (0);
-	// waitpid(pid, &status, 0);
+	int 	status[2];
+	t_command *arg;
+	arg = minishell->command;
+	pid_t pid;
+	while (1)
+	{
+
+		if (pipe(pid) == -1)
+			return 1;
+		pid = fork();
+		if (pid == -1)
+			close_pipe();
+		if (pid == 0)
+			ft_pipe();
+		// if (!redir())
+		// 	return (0);
+		if (!builtins(minishell, arg))
+			return (0);
+		// if (!bin_cmd())
+		// 	return (0);
+		arg = arg->next;
+	}
+		waitpid(pid, &status, 0);
 	return 1;
 }
